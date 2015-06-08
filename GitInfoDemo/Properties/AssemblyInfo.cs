1aed441ce461b83f1204180d92f5cff8e51cf3a0
Daniel Cazzulino <daniel@cazzulino.com>
2015-06-08 00:13:09 -0300

    Updated to show more varied assembly info building

--- GitInfoDemo/Properties/AssemblyInfo.cs
+++ GitInfoDemo/Properties/AssemblyInfo.cs
@@ -1,37 +1,17 @@
 ﻿using System.Reflection;
-using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 using GitInfoDemo;
 
-// General Information about an assembly is controlled through the following 
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
 [assembly: AssemblyTitle ("GitInfoDemo")]
-[assembly: AssemblyDescription ("")]
-[assembly: AssemblyConfiguration ("")]
-[assembly: AssemblyCompany ("")]
 [assembly: AssemblyProduct ("GitInfoDemo")]
-[assembly: AssemblyCopyright ("Copyright ©  2015")]
-[assembly: AssemblyTrademark ("")]
-[assembly: AssemblyCulture ("")]
 
-// Setting ComVisible to false makes the types in this assembly not visible 
-// to COM components.  If you need to access a type in this assembly from 
-// COM, set the ComVisible attribute to true on that type.
 [assembly: ComVisible (false)]
 
-// The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid ("ab166587-551f-44e0-a54b-941e3bd1190d")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion (ThisAssembly.Git.SemVer.Major + "." + ThisAssembly.Git.SemVer.Minor + "." + ThisAssembly.Git.SemVer.Patch)]
-[assembly: AssemblyFileVersion ("1.0.0.0")]
+[assembly: AssemblyVersion (ThisAssembly.Git.BaseVersion.Major + "." + ThisAssembly.Git.BaseVersion.Minor + "." + ThisAssembly.Git.BaseVersion.Patch)]
+[assembly: AssemblyFileVersion (ThisAssembly.Git.SemVer.Major + "." + ThisAssembly.Git.SemVer.Minor + "." + ThisAssembly.Git.SemVer.Patch)]
+[assembly: AssemblyInformationalVersion (
+	ThisAssembly.Git.SemVer.Major + "." + 
+	ThisAssembly.Git.SemVer.Minor + "." + 
+	ThisAssembly.Git.Commits + "-" + 
+	ThisAssembly.Git.Branch + "+" + 
+	ThisAssembly.Git.Commit)]
